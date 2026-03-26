fbec1f04346731bdf6d70dd1d3e11a5a33b189da
Strike32GT <roberto.mas@tecsup.edu.pe>
2026-03-25 23:19:48 -0500

    Tabla productos arreglada 2.0

--- Backend/Backend_Don_Pepe/producto/models.py
+++ Backend/Backend_Don_Pepe/producto/models.py
@@ -6,7 +6,7 @@ class Producto(models.Model):
     nombre = models.CharField(max_length=150)
     precio = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.IntegerField(default=0)
-    categoria = models.ForeignKey(Categoria, on_delete=models.SET_NULL, null=True, related_name='productos', db_column='categoria')
+    categoria = models.ForeignKey(Categoria, on_delete=models.SET_NULL, null=True, related_name='productos', db_column='id_categoria')
     estado = models.BooleanField(default=True)
 
 
